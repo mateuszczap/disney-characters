828b976546cc788650bceed06f15d57af4623353
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-08-09 14:35:15 +0200

    add three same tiles to Favorite area

---
+++ src/components/FavoriteCharacter.jsx
@@ -0,0 +1,23 @@
+export const FavoriteCharacter = () => {
+    return (
+    
+                <div className="favoriteTile">
+                    <div>
+                        <img src="/logo192.png" className="favoriteTileImg"/>
+                    </div>
+                    <div className="mainTile">
+                            <div className="characterNameTile">
+                                <div>Character name</div>
+                                <div>star</div>
+                            </div>
+                        <div>Films:</div>
+                        <div>TV Shows:</div>
+                    </div>
+          
+                    {/* character name */}
+                    {/* star  */}
+                    {/* films - ilosc*/}
+                    {/* Tv shows - ilość */}
+                </div>
+            )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,14 @@
+import { FavoriteCharacter } from "../components/FavoriteCharacter"
+
 export const HomePage = () => {
     return (
     <div>
         <div className="mainContainerFavorite">
             <h1>Most popular Disney Characters</h1>
             <div className="containerFavoriteTiles">
-                <div className="favoriteTile">
-                    <div>
-                        <img src="/logo192.png" className="favoriteTileImg"/>
-                    </div>
-                    <div className="mainTile">
-                            <div className="characterNameTile">
-                                <div>Character name</div>
-                                <div>Star</div>
-                            </div>
-                        <div>Films:</div>
-                        <div>TV Shows:</div>
-                    </div>
-          
-                    {/* character name */}
-                    {/* star  */}
-                    {/* films - ilosc*/}
-                    {/* Tv shows - ilość */}
-                </div>
+            <FavoriteCharacter/>
+            <FavoriteCharacter/>
+            <FavoriteCharacter/>
             </div>
         </div>
 
