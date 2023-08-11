60ee82935f2f8e962d3223b595f5e80ded7f411c
Mateusz Czapiewski <mat.czapiewski@gmail.com>
2023-08-11 17:33:44 +0200

    add some oval style to Disney Character photos and box shadow to thin main container

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,40 +1,40 @@
 import { Star } from "./Star";
 export const CharacterList = (props) => {
-return (
-    <div className="characterList">
-        <h2>Disney Characters</h2>
-        <div className="characterListTitles">
-            <h4>Picture</h4>
-            <h4>Name</h4>
-            <h4>Films count</h4>
-            <h4>Favorite</h4>
-        </div>
-        <div>{props.list.map((character) => {
-            // console.log(props.imageUrl)
-        return (
-            <div className="characterListTitles" key={character._id}>
-                <div>
-                    <img src={props.imageUrl} className="favoriteTileImg" />
-                </div>
-                <div>{character.name}</div>
-                <div>{character.films.length}</div>
-                <div onClick={() => {
-                    
-                    const isExist = props.favoriteCharacters.find((favoriteCharacter) => favoriteCharacter._id===character._id )
-                    if (isExist) {
-                        const newFavoriteCharacters = props.favoriteCharacters.filter((favoriteCharacter) => favoriteCharacter._id!==character._id)
-                        props.setFavoriteCharacters(newFavoriteCharacters)
-                        return 
-                    }
-                    props.setFavoriteCharacters((prev) => {
-                        return [...prev, character]
-                    })
-            }}>
-                <Star isFilled={!!props.favoriteCharacters.find((favoriteCharacter) => favoriteCharacter._id===character._id)}/>
-                </div>
+    return (
+        <div className="characterList">
+            <h2>Disney Characters</h2>
+            <div className="characterListTitles">
+                <h4>Picture</h4>
+                <h4>Name</h4>
+                <h4>Films count</h4>
+                <h4>Favorite</h4>
             </div>
-        )
-    })}</div>
-    </div>
-)
+            <div>{props.list.map((character) => {
+                console.log(props.imageUrl)
+                return (
+                    <div className="characterListTitles" key={character._id}>
+                        <div>
+                            <img src={character.imageUrl} className="characterListImg" />
+                        </div>
+                        <div>{character.name}</div>
+                        <div>{character.films.length}</div>
+                        <div onClick={() => {
+
+                            const isExist = props.favoriteCharacters.find((favoriteCharacter) => favoriteCharacter._id === character._id)
+                            if (isExist) {
+                                const newFavoriteCharacters = props.favoriteCharacters.filter((favoriteCharacter) => favoriteCharacter._id !== character._id)
+                                props.setFavoriteCharacters(newFavoriteCharacters)
+                                return
+                            }
+                            props.setFavoriteCharacters((prev) => {
+                                return [...prev, character]
+                            })
+                        }}>
+                            <Star isFilled={!!props.favoriteCharacters.find((favoriteCharacter) => favoriteCharacter._id === character._id)} />
+                        </div>
+                    </div>
+                )
+            })}</div>
+        </div>
+    )
 };
